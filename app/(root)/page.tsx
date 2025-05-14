5316ef5a77bf63c8efeb556e6720c8132c7c3ac8
b.zakaria <bouzangadzakaria@gmail.com>
2025-05-14 12:57:55 +0100

    add VideoCard component and integrate it into the page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import VideoCard from '@/components/VideoCard'
 import React from 'react'
 
 const Page = () => {
@@ -9,6 +10,18 @@ const Page = () => {
         subHeader='Public Library'
       />
       <h1>welcome</h1>
+
+      <VideoCard 
+        id='1'
+        title="SnapChat Message - 30 June 2025"
+        thumbnail="/assets/samples/thumbnail (1).png"
+        createdAt="2025-06-30T12:00:00Z"
+        userImg="/assets/images/jason.png"
+        username="JOhn Smith"
+        views={15}
+        visibility="public"
+        duration={156}
+      />
 
     </main>
   )
---
+++ components/VideoCard.tsx
@@ -0,0 +1,17 @@
+const VideoCard = ({
+    id,
+    title,
+    thumbnail,
+    createdAt,
+    userImg,
+    username,
+    views,
+    visibility,
+    duration
+}:VideoCardProps) => {
+  return (
+    <div>VideoCard</div>
+  )
+}
+
+export default VideoCard
